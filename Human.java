94def389c225b3e1e94e740b7e329fb7a227f3ee
YongHyun, Lee <48466198+A1paBeta@users.noreply.github.com>
2020-03-28 17:08:08 +0900

    Create Human.java

---
+++ Human.java
@@ -0,0 +1,14 @@
+// 1. 클래스명 : Human (인간)
+// 2. 내부 데이터
+//  - name(이름), String형
+//  - height(키), short형
+
+// 3. 내부 메소드
+//  - public Human(String name) : 생성자, 매개변수를 멤버변수에 대입한 후 super 키워드를 이용하여 Gene 생성자 호출 (호출 시 : 성염색체 1번은 1로, 성염색체 2번은 2로, 기대 키 값은 190으로 한다.)
+//  - public String getName() : 이름을 반환하는 메소드
+//  - public void showInfo() : 성별, 이름, 실제 키를 출력하는 메소
+//  - public boolean isYouHongilDong() : 이름이 '홍길동'이라면 true를 반환, 아니면 false를 반환
+
+public class Human extends Gene{
+
+}
